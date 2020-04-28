ef4255bba3196616b68ce8cbf582a6bf26085269
jakubgierasimczyk <jakub.gierasimczyk@gmail.com>
2020-04-28 21:58:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,134 +1,16 @@
-import secrets
-from typing import Dict, Optional
-
-from fastapi import Depends, FastAPI, Response, status, Request, HTTPException
-from fastapi.templating import Jinja2Templates
-from fastapi.security import APIKeyCookie, HTTPBasic, HTTPBasicCredentials
-from jose import jwt
-from pydantic import BaseModel
-from starlette.responses import RedirectResponse
-
-
-
-class Patient(BaseModel):
-	name: str
-	surname: str
-
-
-class DaftAPI(FastAPI):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.counter: int = 0
-        self.storage: Dict[int, Patient] = {}
-        self.security = HTTPBasic(auto_error=False)
-        self.secret_key = "kluczyk"
-        self.API_KEY = "session"
-        self.cookie_sec = APIKeyCookie(name=self.API_KEY, auto_error=False)
-        self.templates = Jinja2Templates(directory="templates")
-
-
-app = DaftAPI()
-
-
-def is_logged(session: str = Depends(app.cookie_sec), silent: bool = False):
-    try:
-        payload = jwt.decode(session, app.secret_key)
-        return payload.get("magic_key")
-    except Exception:
-        pass
-
-    if silent:
-        return False
-
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-
-def authethicate(credentials: Optional[HTTPBasicCredentials] = Depends(app.security)):
-    if not credentials:
-        return False
-
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-
-    if not (correct_username and correct_password):
-        return False
-    return True
-
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello World during the coronavirus pandemic!"}
-
-
-@app.get("/welcome")
-def welcome(request: Request, is_logged: bool = Depends(is_logged)):
-    return app.templates.TemplateResponse(
-        "welcome.html", {"request": request, "user": "trudnY"}
-    )
-
-
-@app.post("/login")
-async def login_basic(auth: bool = Depends(authethicate)):
-    if not auth:
-        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
-        return response
-
-    response = RedirectResponse(url="/welcome")
-    token = jwt.encode({"magic_key": True}, app.secret_key)
-    response.set_cookie("session", token)
-    return response
-
-
-@app.post("/logout")
-async def logout(is_logged: bool = Depends(is_logged)):
-    response = RedirectResponse(url="/")
-    response.delete_cookie("session")
-    return response
-
-
-@app.post("/patient")
-def add_patient(patient: Patient, is_logged: bool = Depends(is_logged)):
-    app.storage[app.counter] = patient
-    response = RedirectResponse(url=f"/patient/{app.counter}")
-    app.counter += 1
-    return response
-
-
-@app.get("/patient")
-def show_patients(is_logged: bool = Depends(is_logged)):
-    return app.storage
-
-
-@app.get("/patient/{pk}")
-def show_patient(pk: int, is_logged: bool = Depends(is_logged)):
-    if pk in app.storage:
-        return app.storage.get(pk)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.delete("/patient/{pk}")
-def delte_patient(pk: int, is_logged: bool = Depends(is_logged)):
-    if pk in app.storage:
-        del app.storage[pk]
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
-
-
-
-
-# --------------- Lecture 4 --------------- #
-
-
-# ----- Zadanie 1
 import sqlite3
+from fastapi import FastAPI, HTTPException, status
+
+
+
+
+
+app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup():
-    app.db_connection = sqlite3.connect('chinook.db')
+    app.db_connection = sqlite3.connect('chinook_copy.db')
 
 
 @app.on_event("shutdown")
@@ -155,8 +37,6 @@ async def tracks(page: int = 0, per_page: int = 10):
 
 
 
-# ----- Zadanie 2
-
 @app.get("/tracks/composers")
 async def composers(composer_name: str):
 	app.db_connection.row_factory = lambda cursor, x: x[0]
@@ -167,8 +47,8 @@ async def composers(composer_name: str):
     	).fetchall()
 
 	if len(names) == 0:
-		names = {"error": f'No such artist: {composer_name}'}
-		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = names)
+		msg = {"error": f'No such artist: {composer_name}'}
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = msg)
 
 	return names
 
@@ -176,7 +56,9 @@ async def composers(composer_name: str):
 
 
 
-# ----- Zadanie 3
+
+
+
 
 def test_artist_exists(artistid: int):
 
@@ -201,16 +83,22 @@ def test_artist_exists(artistid: int):
 
     return True
 
+
+
+
+
 def get_max_id():
 
     app.db_connection.row_factory = lambda cursor, x: x[0]
     max_id = app.db_connection.execute(
-        '''SELECT max(ArtistId) FROM albums;'''
+        '''SELECT max(AlbumId) FROM albums;'''
         ).fetchall()
 
     return max_id[0]
 
 
+
+from pydantic import BaseModel
 
 
 class NewArtist(BaseModel):
@@ -246,9 +134,12 @@ async def add_album(new_artist: NewArtist):
         "albumid": max_id+1,
         "title": title,
         "artistid": artistid}
+
+    print(f'{new_album=}')
     
     cursor = app.db_connection.execute(
-        "INSERT INTO albums (AlbumId, Title, ArtistId) VALUES (?)", (new_album.albumid, new_album.title, new_album.artistid, )
+        "INSERT INTO albums (AlbumId, Title, ArtistId) VALUES (?, ?, ?)", 
+        (new_album['albumid'], new_album['title'], new_album['artistid'], )
     )
     app.db_connection.commit()  
 
@@ -269,10 +160,13 @@ async def get_album(albumid: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = msg)
 
 
-    album_dict = album
+    # album_dict = album
     album_dict = {
         "AlbumId": album[0][0],
         "Title": album[0][1],
         "ArtistId": album[0][2]}
     
     return album_dict
+
+
+
